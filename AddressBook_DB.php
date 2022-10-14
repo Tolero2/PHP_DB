b0227a466f4dce7d99d7a876da7760e60d407a4b
Tolero2 <110334793+Tolero2@users.noreply.github.com>
2022-10-14 18:01:16 +0200

    Changes to be committed: 	modified:   AddressBook_DB.php

--- AddressBook_DB.php
+++ AddressBook_DB.php
@@ -1,8 +1,5 @@
 <?php
-
-// echo 'this is text';
-
-
+//Connecting to MYSQL Database. DB_Name: 'Address_Book.DB'
 $dsn = 'mysql:dbname=Address_Book;host=127.0.0.1';
 $user = 'root';
 $password = 'MYSQLp@ssword';
@@ -25,16 +22,16 @@ $pdo = new PDO($dsn, $user, $password);
 // print_r($results2);
 
 
-$sts = $pdo->prepare('SELECT * FROM Players WHERE firstName LIKE :fName OR lastName LIKE :lName');
-$sts->execute(
-    [
-        'fName' => '%'.$search.'%',
-        'lName' => '%'.$lastName.'%',
-    ]
-);
+// $sts = $pdo->prepare('SELECT * FROM Players WHERE firstName LIKE :fName OR lastName LIKE :lName');
+// $sts->execute(
+//     [
+//         'fName' => '%'.$search.'%',
+//         'lName' => '%'.$lastName.'%',
+//     ]
+// );
 
-foreach($sts->fetchAll() as $recordNumber => $player) 
-{
-    printf("%d Player: -name- %s, -lastname- %s \n\n", $recordNumber, $player['firstName'], $player['lastName']);
+// foreach($sts->fetchAll() as $recordNumber => $player) 
+// {
+//     printf("%d Player: -name- %s, -lastname- %s \n\n", $recordNumber, $player['firstName'], $player['lastName']);
 
-}
+// }
